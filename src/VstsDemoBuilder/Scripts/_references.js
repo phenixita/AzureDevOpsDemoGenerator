548b2ba11eefed13aa48e88f74ee813aa99126dd
Michele Ferracin <michele.ferracin@live.it>
2026-03-01 23:52:02 +0100

    delete: old web

--- src/VstsDemoBuilder/Scripts/_references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="jquery-1.12.4.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="knockout-2.2.0.debug.js" />
